afa642a327ca317a47bc436f2885f4802a87c0fe
LIANG SHI <a1738982@student.adelaide.edu.au>
2021-02-12 14:35:35 +1030

    filter

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
     <div id="app">
         <div>
-            <input class="mx-auto" v-model="search" placeholder="Restaurant" />
+            <input v-model="search" placeholder="Restaurant" @input="searchAction($event)" />
             <button v-on:click.prevent="handleSearch">搜索</button>
-            <div class="wrapper">
-                <div class="card" v-for="(res, index) in list.restaurants" :key="index">
+            <input type="checkbox" name="hobby" value="健身" />健身
+
+            <div v-if="filterResult.length > 0" class="wrapper">
+                <div class="card" v-for="(res, index) in filterResult" :key="index">
                     <img v-bind:src="res.restaurant.thumb" />
-                    <p>{{ res.restaurant.name }}</p>
+                    {{ res.restaurant.name }}
+                    <small>{{ res.restaurant.cuisines }}</small>
+                    <hr />
+                </div>
+            </div>
+            <div v-else class="wrapper">
+                <div class="card" v-for="(res, index) in list" :key="index">
+                    <img v-bind:src="res.restaurant.thumb" />
+                    {{ res.restaurant.name }}
+                    <small>{{ res.restaurant.cuisines }}</small>
                     <hr />
                 </div>
             </div>
         </div>
+        <!-- <div>{{ id }}</div> -->
     </div>
 </template>
 
@@ -24,8 +36,11 @@ export default {
     data() {
         return {
             search: '',
+            type: '',
+            id: '',
             list: [],
-            searchData: []
+            searchData: [],
+            filterResult: []
         };
     },
     beforeMount() {
@@ -45,14 +60,16 @@ export default {
             })
                 .then(response => {
                     const location = response.data.location_suggestions[0];
+                    this.id = location.entity_id;
+                    this.type = location.entity_type;
                     let res_list = axios({
                         method: 'post',
                         url: `https://developers.zomato.com/api/v2.1/search?entity_id=${location.entity_id}&entity_type=${location.entity_type}`,
                         headers: { 'Content-Type': 'application/json', 'user-key': 'a31bd76da32396a27b6906bf0ca707a2' }
                     })
                         .then(function(res) {
-                            console.log(res.data);
-                            return res.data;
+                            // console.log(res.data);
+                            return res.data.restaurants;
                         })
                         .catch(function(error) {
                             console.log(error);
@@ -62,10 +79,37 @@ export default {
                 })
                 .catch(function(response) {});
             console.log(result);
+
+            return (this.list = result);
+        },
+
+        searchAction(e) {
+            const inputValue = e.target.value.toLowerCase();
+            console.log(inputValue);
+
+            this.filterResult = this.list.filter(item => {
+                const resName = item.restaurant.name.toLowerCase();
+                if (resName.includes(inputValue)) {
+                    return item;
+                }
+            });
+        },
+
+        async handleSearch() {
+            let result = await axios({
+                method: 'post',
+                url: `https://developers.zomato.com/api/v2.1/search?entity_id=${this.id}&entity_type=${this.type}&p=${this.search}`,
+                // data: formData,
+                headers: { 'Content-Type': 'application/json', 'user-key': 'a31bd76da32396a27b6906bf0ca707a2' }
+            })
+                .then(response => {
+                    return response.data;
+                })
+                .catch(res => {
+                    console.log(res);
+                });
             return (this.list = result);
         }
-
-        // async
     }
 };
 </script>
@@ -79,5 +123,10 @@ body {
     flex-direction: column;
     margin-top: 16px;
     margin-bottom: 16px;
+}
+
+small {
+    font-size: 12px;
+    padding: 4px;
 }
 </style>
